b489dcf2edc5ff0d3cb2a58eb476fa427c5e40dd
sashimdr8 <sashimdr8@gmail.com>
2018-04-01 18:50:14 +0545

    updates

--- app/src/main/java/com/ips/MainActivity.java
+++ app/src/main/java/com/ips/MainActivity.java
@@ -4,15 +4,16 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ips.databinding.ActivityLoginBinding;
 import com.ips.databinding.ActivitySignUpBinding;
 
 public class MainActivity extends AppCompatActivity {
-    ActivitySignUpBinding loginBinding;
+    ActivityLoginBinding loginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginBinding = DataBindingUtil.setContentView(this,
-                R.layout.activity_sign_up);
+                R.layout.activity_login);
     }
 }
